2afc324f055f3887c456976fe687a419dd7190d4
hoshianaaa <40942409+hoshianaaa@users.noreply.github.com>
2019-12-17 13:09:35 +0900

    Update rename.sh

--- rename.sh
+++ rename.sh
@@ -1,2 +1,2 @@
-ls | awk '{ printf "mv %s test%03d.png\n", $0, NR+150 }'
-ls | awk '{ printf "mv %s test%03d.png\n", $0, NR+150 }' | sh
+ls | awk '{ printf "mv %s image%03d.jpg\n", $0, NR+150 }'
+ls | awk '{ printf "mv %s image%03d.jpg\n", $0, NR+150 }' | sh
